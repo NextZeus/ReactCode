2eb30a728718f7c6acd9c1c295fa3478e0420587
xiaodong <875697126@qq.com>
2016-08-10 18:44:24 +0800

    update

--- processBarAndroid.js
+++ processBarAndroid.js
@@ -23,3 +23,35 @@ render: function() {
         <ProgressBarAndroid styleAttr='Inverse'/>
 </View>
 */
+
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  ProgressBarAndroid,
+} from 'react-native';
+ 
+class ProgressBarDemo extends Component {
+  render() {
+    return (
+      <View >
+        <Text>
+            ProgressBarAndroid控件实例
+        </Text>
+        <ProgressBarAndroid  color="red" styleAttr='Inverse'/>
+        <ProgressBarAndroid  color="green" styleAttr='Horizontal' progress={0.2} 
+            indeterminate={false} style={{marginTop:10}}/>
+        <ProgressBarAndroid  color="green" styleAttr='Horizontal'
+            indeterminate={true} style={{marginTop:10}}/>
+        <ProgressBarAndroid  color="black" styleAttr='SmallInverse'
+            style={{marginTop:10}}/>
+        <ProgressBarAndroid  styleAttr='LargeInverse'
+            style={{marginTop:10}}/>
+      </View>
+    );
+  }
+}
+AppRegistry.registerComponent('ProgressBarDemo', () => ProgressBarDemo);
