7ce75b4ba4f9af6f78c4bdf34921c45a63065068
xiaodong <875697126@qq.com>
2016-08-10 18:45:24 +0800

    update

--- processBarAndroid.js
+++ processBarAndroid.js
@@ -22,6 +22,25 @@ render: function() {
         </Text>
         <ProgressBarAndroid styleAttr='Inverse'/>
 </View>
+
+属性方法
+
+3.1.支持View控件的属性方法 (这些属性是从View控件中继承下来)  例如:大小,布局,边距啊
+
+3.2.color  设置进度的颜色属性值
+
+3.3.indeterminate 设置是否要显示一个默认的进度信息，该如果styleAttr的风格设置成Horizontal的时候该值必须设置成false
+
+3.4.progress  number  设置当前的加载进度值(该值在0-1之间)
+
+3.5.styleAttr    进度条框的风格 ，可以取的值如下:
+
+Horizontal
+Small
+Large
+Inverse
+SmallInverse
+LargeInverse
 */
 
 'use strict';
