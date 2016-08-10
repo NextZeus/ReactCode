7e704ccbab78cc837ae5864a7665cfb48c58228d
xiaodong <875697126@qq.com>
2016-08-10 18:43:02 +0800

    processbar

---
+++ processBarAndroid.js
@@ -0,0 +1,17 @@
+/*
+官方的写的一个例子
+render: function() {
+  var progressBar =
+    <View style={styles.container}>
+      <ProgressBar styleAttr="Inverse" />
+    </View>;
+ 
+  return (
+    <MyLoadingComponent
+      componentView={componentView}
+      loadingView={progressBar}
+      style={styles.loadingComponent}
+    />
+  );
+},
+*/
