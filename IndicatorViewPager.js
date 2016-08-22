710a2049e9c6a8b00c02a6867e0a51da231a8f29
xiaodong <875697126@qq.com>
2016-08-22 11:44:00 +0800

    add SegmentedControlIOS

---
+++ IndicatorViewPager.js
@@ -0,0 +1,17 @@
+/**
+* 轮播图
+*/
+import React from 'react';
+import {IndicatorViewPager, PageDotIndicator} from 'rn-viewpager'
+
+class IndicatorViewPagerDemo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayName = 'IndicatorViewPagerDemo';
+    }
+    render() {
+        return <div>IndicatorViewPagerDemo</div>;
+    }
+}
+
+export default IndicatorViewPagerDemo;
---
+++ SegmentedControlIOS.js
@@ -0,0 +1,62 @@
+/**
+ * Created by lixiaodong on 16/8/22.
+ */
+'use strict'
+
+import React,{
+    AppRegistery,
+    Component,
+    StyleSheet,
+    Text,
+    View,
+    SegmentedControlIOS
+} from 'react-native';
+const styles = StyleSheet.create({
+    welcome:{
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    segmentedControl1:{
+        margin:10,
+        height:30,
+        width:200,
+        alignSelf:'center'
+    },
+    segmentedControl2:{
+        marginTop:20,
+        margin:10,
+        height:30,
+        width:300,
+        alignSelf:'center'
+    }
+});
+
+const segmentedControl1 = ['全国', '南通'];
+const segmentedControl2 = ['Android', 'iOS','Java','React'];
+
+
+
+class SegmentedControlIOSDemo extends Component{
+    render(){
+        return (
+            <View>
+                <Text style={styles.welcome}>
+                    SegmentedControlIOS使用实例
+                </Text>
+                <SegmentedControlIOS
+                    values={["全国","南通"]}
+                    tintColor="red"
+                    style={styles.segmentedControl1}
+                    />
+                    <SegmentedControlIOS
+                        values={segmentedControl2}
+                        tintColor="green"
+                        selectedIndex={1}
+                        onValueChange={(value)=>console.log('选中了'+value)}
+                        style={styles.segmentedControl2}
+                        />
+            </View>
+        );
+    }
+}
